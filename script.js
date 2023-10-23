1ac944230cae2482e820a870f6af4dfe41ea477a
Vitor2008 <gabrielronaldo9343@gmail.com>
2023-10-23 18:38:52 -0400

    novo projeto

--- script.js
+++ script.js
@@ -44,11 +44,11 @@ ScrollReveal({
     reset: true,
     distance: '80px',
     duration: 2000,
-    delay: 200
+    delay: 200,
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form, .img', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
